fba788299e4f24cb5447e33cbaae55cbddf00c4c
CengizhanOemuer <cengiz.cmr@gmail.com>
2022-10-31 16:06:53 +0100

    LinearSearch hinzugefügt!

---
+++ VS-Code/Java/Algorithmen/Search/LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,9,8,7,4,6,5};
+        System.out.println(linearSearch(arr, 10));
+    }
+
+    // Iterative:
+    public static boolean linearSearch(int[] array, int num) {
+        for(int i=0; i < array.length-1; i++) {
+            if(array[i] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
