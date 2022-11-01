70048b4ad6eba5a801a3efedfb897fe7bf965ef0
CengizhanOemuer <cengiz.cmr@gmail.com>
2022-11-01 13:19:50 +0100

    Bug gefixt

--- VS-Code/Java/Algorithmen/Sort/Quicksort.java
+++ VS-Code/Java/Algorithmen/Sort/Quicksort.java
@@ -3,51 +3,53 @@ package Java.Algorithmen.Sort;
 class Quicksort {
     public static void main(String[] args) {
         int[] arr = {40, 20, 7, 80, 60, 50, 10, 30, 17, 42};
-        // Ausgabe:
-        for(int i = 0; i < arr.length-1; i++) {
-            System.out.println("Unsortiertes Array:" + arr[i]);
-        }
-        quickSort(arr, 0, arr.length-1);
-        for(int i = 0; i < arr.length-1; i++) {
-            System.out.println("Sortiertes Array:" + arr[i]);
-        }
+        printArray(arr);
+        quicksort(arr, 0, arr.length-1);
+        System.out.println("--------------------------------------------------------");
+        printArray(arr);
     }
 
-    // Methode zum vertauschen der Array-Elemente:
-    static void swap(int[] arr, int i, int j) {
+    // Quicksort-Methode:
+    private static void quicksort(int[] arr, int lowIndex, int highIndex) {
+
+        // Rekursionsanker:
+        if(lowIndex >= highIndex)
+            return;
+
+        // Festlegen des Pivots:
+        int pivot = arr[highIndex];
+        // Pointer erzeugen:
+        int leftPointer = lowIndex;
+        int rightPointer = highIndex;
+
+        while (leftPointer < rightPointer) {
+
+            while(arr[leftPointer] <= pivot && leftPointer < rightPointer) {
+                leftPointer++;   
+            }
+            while(arr[rightPointer] >= pivot && leftPointer < rightPointer) {
+                rightPointer--;
+            }
+            swap(arr, leftPointer, rightPointer);
+        }
+        swap(arr, leftPointer, highIndex);
+
+        // Rekursiveraufruf:
+        quicksort(arr, lowIndex, leftPointer - 1);
+        quicksort(arr, leftPointer + 1, highIndex);
+    }
+    
+    // Swap-Methode:
+    private static void swap(int[] arr, int i, int j) {
         int help = arr[i];
         arr[i] = arr[j];
         arr[j] = help;
     }
-    // Methode zur Partionierung des Arrays:
-    static int partition(int[] arr, int low, int high) {
-        // Festlegen eines Pivots:
-        int pivot = arr[high];
-        //
-        int i = (low-1);
-        //
-        for(int j = low; j <= high - 1; j++) {
-            // Falls arr[j] kleiner als Pivot ist:
-            if(arr[j] < pivot) {
-                i++; // Erhöhe den Index vom kleineren Element!
-                swap(arr, i, j);
-            }
-        }
-        swap(arr, i + 1, high);
-        return (i + 1);
-    }
-    // 
-    static void quickSort(int[] arr, int low, int high) {
-        
-        if(low < high) {
-            // pi steht für den Partionierungs Index
-            int pi = partition(arr, low, high);
 
-            // 
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1 , high);
+    // Array ausgeben:
+    private static void printArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println("Array[" + i + "] = " + arr[i]);
         }
     }
-
-
 }
