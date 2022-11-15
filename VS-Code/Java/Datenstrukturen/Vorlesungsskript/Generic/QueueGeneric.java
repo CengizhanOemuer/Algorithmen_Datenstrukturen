78953b21c73f709451eb09c696e54dc645844ed9
CengizhanOemuer <cengiz.cmr@gmail.com>
2022-11-15 22:14:51 +0100

    Generic-Queue hinzugefügt!

--- VS-Code/Java/Datenstrukturen/Vorlesungsskript/Generic/QueueGeneric.java
+++ VS-Code/Java/Datenstrukturen/Vorlesungsskript/Generic/QueueGeneric.java
@@ -1,5 +1,45 @@
 package Java.Datenstrukturen.Vorlesungsskript.Generic;
 
-public class QueueGeneric {
-    
+import java.util.NoSuchElementException;
+
+public class QueueGeneric<T> {
+    // Attribute:
+    private KnotenGeneric<T> tail = null;
+    private KnotenGeneric<T> head = null;
+
+    // Enqueue_Method fügt ein Element am Ende der Queue hinzu:
+    public QueueGeneric<T> enqueue(T element) {
+        KnotenGeneric<T> oldTail = tail;
+        tail = new KnotenGeneric<T>(element, null);
+        if(head != null) {
+            oldTail.setNext(tail);
+        } else {
+            head = tail;
+        }
+        return this;
+    }
+
+    // Dequeue_Method entfernt das vorderste Element in der Queue:
+    public QueueGeneric<T> dequeue() {
+        if(!isEmpty()) {
+            head = head.getNext();
+        } else {
+            throw new NoSuchElementException();
+        }
+        return this;
+    }
+
+    // Front_Method gibt das vorderste Element zurück ohne dieses zu entfernen:
+    public T front() {
+        if(!isEmpty()) {
+            return head.getElement();
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    // IsEmpty_Method guckt ob die Queue leer ist:
+    private boolean isEmpty() {
+        return head == null;
+    }
 }
--- VS-Code/Java/Datenstrukturen/Vorlesungsskript/Generic/TestMain.java
+++ VS-Code/Java/Datenstrukturen/Vorlesungsskript/Generic/TestMain.java
@@ -11,5 +11,16 @@ public class TestMain {
         myStack.pop();
         System.out.println("Das oberste Element ist: " + myStack.top());
         myStack.pop();
+
+        // Generic-Queue Test:
+        QueueGeneric<Integer> myQueue = new QueueGeneric<>();
+        myQueue.enqueue(1);
+        System.out.println("Das vorderste Element ist: " + myQueue.front());
+        myQueue.enqueue(3);
+        System.out.println("Das vorderste Element ist: " + myQueue.front());
+        myQueue.dequeue();
+        System.out.println("Das vorderste Element ist: " + myQueue.front());
+        myQueue.dequeue();
+        System.out.println("Das vorderste Element ist: " + myQueue.front());
     }
 }
