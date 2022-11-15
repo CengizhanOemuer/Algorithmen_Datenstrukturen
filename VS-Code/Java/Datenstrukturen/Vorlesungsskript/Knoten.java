708f2d2ec33a3992c0a48f8bf4cada14ee3fcdc5
CengizhanOemuer <cengiz.cmr@gmail.com>
2022-11-15 20:33:47 +0100

    Knoten hinzugefügt!

---
+++ VS-Code/Java/Datenstrukturen/Vorlesungsskript/Knoten.java
@@ -0,0 +1,30 @@
+package Java.Datenstrukturen.Vorlesungsskript;
+
+public class Knoten {
+    // Attribute:
+    private Object object;          // Nimmt ein Objekt vom Datentyp object entgegen!
+    private Knoten next;            // Verweist auf das nächste Knotenelement!
+
+    // Konstruktor:
+    public Knoten(Object object, Knoten next) {
+        this.object = object;
+        this.next = next;
+    }
+
+    // Methoden:
+
+    // Setzt den Verweis auf das nächste Knotenelement:
+    public void setNext(Knoten next) {
+        this.next = next;
+    }
+
+    // Gibt den Knoten zurück:
+    public Object getKnoten() {
+        return object;
+    }
+
+    // Gibt den nächsten Knoten zurück:
+    public Knoten getNext() {
+        return next;
+    }
+}
---
+++ VS-Code/Java/Datenstrukturen/Vorlesungsskript/KnotenGeneric.java
@@ -0,0 +1,31 @@
+package Java.Datenstrukturen.Vorlesungsskript;
+
+public class KnotenGeneric<T> {
+    // Attribute:
+    private T object;                       // Nimmt ein Objekt vom Datentyp "T" entgegen!
+    private KnotenGeneric<T> next;          // Verweist auf das nächste Element in der Datenstruktur!
+
+    // Konstruktor:
+    public KnotenGeneric(T object, KnotenGeneric<T> next) {
+        this.object = object;
+        this.next = next;
+    }
+
+    // Methoden:
+
+    // Setzt den Verweis auf den nächsten Knoten:
+    public void setNext(KnotenGeneric<T> next) {
+        this.next = next;
+    }
+
+    // Gibt das Knotenelement zurück:
+    public T getKnoten() {
+        return object;
+    }
+
+    // Gibt das nächste Knotenelement zurück:
+    public KnotenGeneric<T> getNext() {
+        return next;
+    }
+
+}
